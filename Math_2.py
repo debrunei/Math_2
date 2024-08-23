4d78f834b8fdae25bd7760f8fe8384cfaddaa43a
debrunei000 <debrunei000@warren.k12.in.us>
2024-08-23 10:11:52 -0400

    First commit

---
+++ Math_2.py
@@ -0,0 +1,7 @@
+miles = 5
+minutes = 45
+hours = minutes / 60
+mph = miles / hours
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print("Speed in mph", mph)
